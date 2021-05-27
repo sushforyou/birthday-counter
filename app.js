4a332cc366d07d4de5b85e757f69dda644382828
[PriyankaBodake] <[priyankabodake16@gmail.com]>
2021-05-27 22:47:07 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const timeLeft = document.getElementById('time-left');
+
+const birthday = new Date('06/16/2021');
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+let timerId;
+
+function countDown() {
+    const today = new Date();
+    const timeSpan = birthday - today;
+    if(timeSpan <= -day) {
+        timeLeft.innerHTML = 'Hope you had nice Birthday &#128516;';
+        clearInterval(timerId);
+        return;
+    }
+    if(timeSpan <= 0 ) {
+        timeLeft.innerHTML = `Happy Birthday Priyanka&#127874; \n वाढदिवसाच्या हार्दिक शुभेच्छा ❤`;
+        clearInterval(timerId);
+        return;
+    }
+ 
+    const days = Math.floor(timeSpan/day);
+    const hours =  Math.floor((timeSpan % day) / hour);
+    const minutes =  Math.floor((timeSpan % hour) / minute);
+    const seconds =  Math.floor((timeSpan % minute) / second);
+
+    timeLeft.innerHTML = ` ${days} days ${hours} hours ${minutes}  minutes ${seconds} seconds`;
+
+}
+
+timerId = setInterval(countDown,1000);
